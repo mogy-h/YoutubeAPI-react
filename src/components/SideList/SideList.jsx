b80688bab6a4403d38705682d9f8bbd379278794
mogy-h <rapter.mg@gmail.com>
2021-09-04 21:06:47 +0900

    thumbnails of undefined になるのを解決
    videp.snippetが存在するときにだけSideListItemコンポーネントをreturnするようにした

--- src/components/SideList/SideList.jsx
+++ src/components/SideList/SideList.jsx
@@ -2,38 +2,37 @@ import React, { useEffect, useContext } from "react";
 import { Store } from "../../store/index";
 import { fetchRelatedData } from "../../apis/index";
 import SideListItem from "../SideListItem/SideListItem";
+import Style from "./SideList.module.scss";
 
 const SideList = () => {
   const { globalState, setGlobalState } = useContext(Store);
   const setRelatedVideo = async (id) => {
     await fetchRelatedData(id).then((res) => {
-      setGlobalState({
-        type: "SET_RELATED",
-        payload: { related: res.data.items },
-      });
+      setGlobalState({ type: "SET_RELATED", payload: { related: res.data.items } });
     });
   };
 
   useEffect(() => {
-    const unSub = setRelatedVideo(globalState.selected.id);
-
-    return () => {
-      unSub();
-    };
+    setRelatedVideo(globalState.selected.id);
     //eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [globalState.selected]);
 
   return (
-    <div>
+    <div className={Style.sidenav}>
       {globalState.related ? (
+        // eslint-disable-next-line array-callback-return
         globalState.related.map((video) => {
+          console.log(video.snippet.thumbnails);
+
           return (
-            <SideListItem
-              id={video.id.videoId}
-              key={video.id.videoId}
-              src={video.snippet.thumbnails.medium.url}
-              title={video.snippet.title}
-            />
+            video.snippet && (
+              <SideListItem
+                id={video.id.videoId}
+                key={video.id.videoId}
+                src={video.snippet.thumbnails.medium.url}
+                title={video.snippet.title}
+              />
+            )
           );
         })
       ) : (
--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import Layout from "../components/Layout/Layout";
 import SideList from "../components/SideList/SideList";
 import VideoDetail from "../components/VideoDetail/VideoDetail";
+import { Store } from "../store";
+import { useLocation } from "react-router";
+import { fetchSelectedData, fetchRelatedData } from "../apis";
 
 const Watch = () => {
+  const { globalState, setGlobalState } = useContext(Store);
+  const location = useLocation();
+  const setVideos = async () => {
+    const searchParams = new URLSearchParams(location.search);
+    const id = searchParams.get("v");
+    if (id) {
+      const result = await Promise.all([fetchSelectedData(id), fetchRelatedData(id)]);
+      console.log(result);
+    }
+  };
+
+  useEffect(() => {
+    setVideos()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, location.search)
+
   return (
     <Layout>
       <VideoDetail />
-      <SideList/>
+      <SideList />
     </Layout>
   );
 };
